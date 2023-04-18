c9e64242c9cab4f1008133117f6b7ae87c8f04d6
Jace Cockrell <jace.c.IT@gmail.com>
2023-04-18 07:54:25 -0500

    finished the best i could

--- Block1/VariablesLab.cs
+++ Block1/VariablesLab.cs
@@ -28,6 +28,37 @@ namespace Block1
             long neg = -9000000000000000000;
             long pos = 9000000000000000000;
             Console.WriteLine(loving + neg + pos);
+            string first = "float:";
+            float fOne = 1.1f;
+            float fTwo = 2.2f;
+            Console.WriteLine(first + fOne);
+            Console.WriteLine(first + fTwo);
+            string day = "double:";
+            double dOne = 10.1;
+            double dTwo = 20.2;
+            Console.WriteLine(day + dOne);
+            Console.WriteLine(day + dTwo);
+            string did = "decimal:";
+            decimal mOne = 10.10m;
+            decimal mTwo = 20.20m;
+            Console.WriteLine(did + mOne);
+            Console.WriteLine(did + mTwo);
+            string lett = "char:";
+            char cOne = 'a';
+            char cTwo = 'b';
+            Console.WriteLine(lett + cOne);
+            Console.WriteLine(lett + cTwo);
+            string sup = "string:";
+            string sOne = "This is a string.";
+            string sTwo = "This is also a string.";
+            Console.WriteLine(sup + sOne);
+            Console.WriteLine(sup + sTwo);
+            string boy = "bool:";
+            bool bOne = false;
+            bool bTwo = true;
+            Console.WriteLine(boy + bOne);
+            Console.WriteLine(boy + bTwo);
+
 
 
 

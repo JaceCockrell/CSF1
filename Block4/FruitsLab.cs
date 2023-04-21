958c4109092eb53337c7ad559136c54d23a385be
Jace Cockrell <jace.c.IT@gmail.com>
2023-04-21 09:50:12 -0500

    did some labs

--- Block4/FruitsLab.cs
+++ Block4/FruitsLab.cs
@@ -17,7 +17,7 @@ namespace Block4
             fruit[2] = "Strawberry";
             fruit[3] = "Raspberry";
             fruit[4] = "Dragon Fruit";
-            Console.WriteLine(fruit[0].Substring(0, 1));
+            Console.WriteLine(fruit[0].Substring(0, fruit[0].IndexOf('a')));
             Console.WriteLine(fruit[1].Substring(0, 2));
             Console.WriteLine(fruit[2].Substring(0, 3));
             Console.WriteLine(fruit[3].Substring(0, 1));

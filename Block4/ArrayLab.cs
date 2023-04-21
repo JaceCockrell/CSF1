812f486db14ca70f1ff4803a7f4236f2954d4176
Jace Cockrell <jace.c.IT@gmail.com>
2023-04-20 21:01:22 -0500

    did array and fruits

---
+++ Block4/ArrayLab.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Block4
+{
+    internal class ArrayLab
+    {
+        static void Main(string[] args)
+        {
+            int[] scores = new int[5];
+            Console.WriteLine("Give the first of 5 test scores.");
+            scores[0] = int.Parse(Console.ReadLine());
+            Console.WriteLine("Give the second of 5 test scores.");
+            scores[1] = int.Parse(Console.ReadLine());
+            Console.WriteLine("Give the third of 5 test scores.");
+            scores[2] = int.Parse(Console.ReadLine());
+            Console.WriteLine("Give the fourth of 5 test scores.");
+            scores[3] = int.Parse(Console.ReadLine());
+            Console.WriteLine("Give the last of 5 test scores.");
+            scores[4] = int.Parse(Console.ReadLine());
+            Array.Sort(scores);
+            Console.WriteLine($"{scores[0]} is the lowest score, {scores[4]} is the highest score.");
+            int allScores = scores.Sum();
+            allScores = allScores / 5;
+            Console.WriteLine($"Your average score was: {allScores}");
+
+
+
+
+
+
+        }//main
+    }//class
+}//name
---
+++ Block4/FruitsLab.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Block4
+{
+    internal class FruitsLab
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fruits lab");
+            string[] fruit = new string[5];
+            fruit[0] = "Banana";
+            fruit[1] = "Grape";
+            fruit[2] = "Strawberry";
+            fruit[3] = "Raspberry";
+            fruit[4] = "Dragon Fruit";
+            Console.WriteLine(fruit[0].Substring(0, 1));
+            Console.WriteLine(fruit[1].Substring(0, 2));
+            Console.WriteLine(fruit[2].Substring(0, 3));
+            Console.WriteLine(fruit[3].Substring(0, 1));
+            Console.WriteLine(fruit[4].Substring(0, 2));
+
+
+
+
+        }//end main
+    }//end class
+}//end name
